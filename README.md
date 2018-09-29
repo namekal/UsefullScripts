c2ff127c043de287b505bf5bc9413e7a4e52e689
XLNCs <aryank2908@gmail.com>
2018-09-30 01:06:20 +0530

    added cpunamefix.sh

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ A repo of small useful scripts that i wrote.
 
 - [`sysinfo`](sysinfo.sh) :`Provides a summary of information about macOS system.`
 
+<<<<<<< HEAD
 ```bash
 ###Usage
 bash <(curl -s https://raw.githubusercontent.com/XLNCs/UsefullScripts/master/sysinfo.sh)
@@ -10,3 +11,22 @@ bash <(curl -s https://raw.githubusercontent.com/XLNCs/UsefullScripts/master/sys
 
 ![sysinfosc](Images/sysinfosc.png)
 
+----------
+
+- [`cpunamefix`](cpunamefix.sh) :`Set the correct or a custom CPU name in 'About This Mac`
+
+![sysinfosc](Images/cpunamefixsc.png)
+
+```bash
+###Usage
+bash <(curl -s https://raw.githubusercontent.com/XLNCs/UsefullScripts/master/cpunamefix.sh)
+```
+=======
+```bash
+###Usage
+bash <(curl -s https://raw.githubusercontent.com/XLNCs/UsefullScripts/master/sysinfo.sh)
+```
+
+![sysinfosc](Images/sysinfosc.png)
+
+>>>>>>> 8ae38d670e287c50c63c29aca7eb533163922ef7
---
+++ cpunamefix.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+##Name : cpunamefix.sh
+##Info : Script to fix CPU name in 'About This Mac' on macOS system.
+##Auth : XLNC
+## Date : 29/09/2018
+
+DIRLOCATION="/System/Library/PrivateFrameworks/AppleSystemInfo.framework/Versions/A/Resources/"
+LIST=($(cd ${DIRLOCATION} && ls -1d */ | cut -d\/ -f1))
+REALCPUNAME="$(sysctl -n machdep.cpu.brand_string)"
+CURRENTCPUNAME="$(/usr/libexec/PlistBuddy -c "Print :UnknownCPUKind" ${DIRLOCATION}/English.lproj/AppleSystemInfo.strings)"
+ITL='\033[3m'
+BOLD='\033[1m'
+STD='\033[0m'
+
+printf '\033[8;30;100t' && clear
+echo -e "\n${ITL}  CPU Name Fix ~XLNC~ ${STD}"
+echo -e "${ITL}  $(date -R) ${STD}"
+sleep 1.5
+
+echo -e "\n\nCurrent CPU name : ${BOLD}${CURRENTCPUNAME}${STD}"
+echo -e "Actual CPU name  : ${BOLD}${REALCPUNAME}${STD}\n"
+sleep 1.5
+
+while ! [[ ${REPLY} =~ ^[Yy|Nn]$ ]]; do
+	echo -e "Do you want to use a custom CPU name or the actual CPU name ?"
+	read -p "[Custom=Y / Actual=N]: " REPLY
+	if [[ ${REPLY} =~ ^[Yy]$ ]]; then
+		echo
+		read -p "Enter custom CPU name : " CPUNAME
+		echo -e "\nApplying custom CPU name : ${BOLD}${CPUNAME}${STD}\n"
+	elif [[ ${REPLY} =~ ^[Nn]$ ]]; then
+		echo -e "\nApplying actual CPU name : ${BOLD}${REALCPUNAME}${STD}\n"
+		CPUNAME="${REALCPUNAME}"
+	else
+		echo -e "\n[x] Error : Enter only Y or N\n"
+	fi
+done
+
+for ITEM in "${LIST[@]}"; do
+
+	if [ ! -e "${DIRLOCATION}${ITEM}/AppleSystemInfo.strings.backup" ]; then
+		sudo cp -Rf ${DIRLOCATION}${ITEM}/AppleSystemInfo.strings ${DIRLOCATION}${ITEM}/AppleSystemInfo.strings.backup
+	fi
+	if [ -e "${DIRLOCATION}${ITEM}/AppleSystemInfo.strings.backup" ]; then
+		sudo rm -rf ${DIRLOCATION}${ITEM}/AppleSystemInfo.strings
+		sudo cp -Rf ${DIRLOCATION}${ITEM}/AppleSystemInfo.strings.backup ${DIRLOCATION}${ITEM}/AppleSystemInfo.strings
+	fi
+
+	FILE="${DIRLOCATION}${ITEM}/AppleSystemInfo.strings"
+
+	function bintoxml() {
+		sudo /usr/bin/plutil -convert xml1 "$1"
+	}
+	function xmltobin() {
+		sudo /usr/bin/plutil -convert binary1 "$1"
+	}
+
+	bintoxml ${FILE}
+	sudo sed -i '' "s/\\<string\\>Unknown\\<\\/string\\>/\\<string\\>$CPUNAME\\<\\/string\\>/" ${FILE}
+	xmltobin ${FILE}
+
+done
+
+echo -e "\nDone.\n"
